3af3b748e74262e56f057a684dc25388f6a2c9d6
Matthew Cavallo <macavall@microsoft.com>
2023-10-23 17:09:11 -0500

    Add project files.

---
+++ Controllers/Home2Controller.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InterviewMvc.Controllers
+{
+    public class Home2Controller : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            ProcessFunction();
+
+            return "Command Executed";
+        }
+
+        private void ProcessFunction()
+        {
+            try
+            {
+                ProcessFunction();
+            }
+            finally
+            {
+                ProcessFunction();
+            }
+        }
+    }
+}
---
+++ Controllers/Home3Controller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace InterviewMvc.Controllers
+{
+    public class Home3Controller : Controller
+    {
+        ArrayList objectList = new ArrayList();
+
+        private Array SaveObject(int objectId)
+        {
+            int numbers = objectId / 8;
+            int[] arr = new int[numbers];
+
+            for (int i = 0; i < numbers; i++)
+            {
+                arr[i] = i;
+            }
+
+            return arr;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var id = 400000 * 1024;
+            var objectId = SaveObject(id);
+            objectList.AddRange(objectId);
+            return "Command Executed";
+        }
+    }
+}
